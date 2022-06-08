f8ed2113e6512167218a9890583b36355ea83e3d
mahama <958421714@qq.com>
2022-06-08 14:26:07 +0800

    更新bouncycastle

--- src/main/java/com/mahama/common/utils/SslUtil.java
+++ src/main/java/com/mahama/common/utils/SslUtil.java
@@ -1,16 +1,18 @@
 package com.mahama.common.utils;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.openssl.PEMReader;
+import org.bouncycastle.util.io.pem.PemReader;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManagerFactory;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.*;
 import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 
 public class SslUtil {
@@ -23,8 +25,11 @@ public class SslUtil {
         TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
         if (null != caCrtFile) {
             // 加载本地指定的 ca 证书
-            PEMReader reader = new PEMReader(new InputStreamReader(caCrtFile));
-            X509Certificate caCert = (X509Certificate) reader.readObject();
+            PemReader reader = new PemReader(new InputStreamReader(caCrtFile));
+            byte[] content = reader.readPemObject().getContent();
+            CertificateFactory factory = CertificateFactory.getInstance("X.509");
+            ByteArrayInputStream in = new ByteArrayInputStream(content);
+            X509Certificate caCert = (X509Certificate) factory.generateCertificate(in);
             reader.close();
 
             // CA certificate is used to authenticate server
