a9e68503afd03e2e728d3c60f576dedbbb9d8555
mahama <958421714@qq.com>
2023-05-30 14:20:30 +0800

    JSONUtil增加配置

--- src/main/java/com/mahama/common/utils/JSONUtil.java
+++ src/main/java/com/mahama/common/utils/JSONUtil.java
@@ -1,9 +1,23 @@
 package com.mahama.common.utils;
 
+import com.alibaba.fastjson.PropertyNamingStrategy;
+import com.alibaba.fastjson.parser.ParserConfig;
 import com.alibaba.fastjson.serializer.PropertyFilter;
 
 public class JSONUtil {
     public static PropertyFilter auditorFilter(){
         return  (source, name, value) -> !StringUtil.checkRegex(name,"(modify|create)\\w+");
+    }
+
+    public static ParserConfig camelCase() {
+        ParserConfig parserConfig = new ParserConfig();
+        parserConfig.propertyNamingStrategy = PropertyNamingStrategy.CamelCase;
+        return parserConfig;
+    }
+
+    public static ParserConfig snakeCase() {
+        ParserConfig parserConfig = new ParserConfig();
+        parserConfig.propertyNamingStrategy = PropertyNamingStrategy.SnakeCase;
+        return parserConfig;
     }
 }
